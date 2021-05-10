1ec16be5dc578cdd3d3f448c1713b6fdf1b17d3b
Antonio colli <36493960+antonioCC@users.noreply.github.com>
2021-05-10 12:33:06 -0500

    Update programa T2.py

--- programa T2.py
+++ programa T2.py
@@ -3,6 +3,7 @@ import re
 archivo=open("datosx.txt","r")
 mensaje=archivo.read()
 
+#se inician las list yvariables a utilizar
 lista = []
 lista2 = []
 lista3 =[]
@@ -10,18 +11,16 @@ m = 0
 m1=0
 var = ""
 
-#patron = re.compile('(([A-Za-z]+\w?|\d{1,7})[\+|\-|\*|\/]([A-Za-z]+\w?|\d{1,7}))')# expresion regular
-patron = re.compile('\w+\s\=\s\w+\s[+*-/]\s\w+\s[+*-/]\s\w')
-a=str(mensaje)
 
+#patron = re.compile('\w+\s\=\s\w+\s[+*-/]\s\w+\s[+*-/]\s\w')
+a=str(mensaje)#se guarda en a lo que hay en el txt
+
+#ciclo for para pasar cada elemento de a en lista
 for b in a:
 	lista.append(b)
 
-#lista.append(mensaje)
-#a=a.strip("[").strip("]")
-#c=c.replace("e","a")
-#lista.remove("5")
-m1=0
+#ciclo for para verificar si encontro un [+-/*], si lo encuentra verifica que su sucesor y antecesor no esten vacios
+#para poder asignar a var el triplo, ingresarlo en lista2 y al final quita los elementos que tomo de lista
 for n in a:
 	if n == "*" or n == "/" or n == "+" or n == "-":
 		if  lista[m1-2]!=" " and lista[m1+2]!=" ":
@@ -34,23 +33,23 @@ for n in a:
 	m1+=1
 	#print(m1,n)
 
+#en este ciclo verifica si los elementos que quedan cumplen las condiciones para relizar un ultimo triplo usando el triplo anterior
+#para luego remover tambien los elentos que tomo
 m1=0
 for c in lista:
 	if re.match('[+*-/]',c) and lista[m1+2]==" ":
-		#print(m1,": si paso",c,"element ",lista[m1+2])
 		var = "t"+str(m),"= ",lista[m1-2],c,"t"+str(m-1)
 		lista2.append(var)
 		lista.remove(lista[m1-2])
 		lista.remove(c)
-		#lista.remove(lista[m1+2])
 	elif re.match('[+*-/]',c) and lista[m1-2]==" ":
 		var = "t"+str(m),"= ",lista[m1+2],c,"t"+str(m-1)
 		lista2.append(var)
-		#lista.remove(lista[m1-2])
 		lista.remove(c)
 		lista.remove(lista[m1+1])
 	m1+=1
 
+#imprime los elementos de lista2
 d=None
 for d in lista2:
 	print(d)
